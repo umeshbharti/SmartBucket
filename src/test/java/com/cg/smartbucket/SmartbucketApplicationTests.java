62b36f9381a22d6e56df79ab3f07f2d1b72da3be
Umesh Bharti <umesh.bharti@capgemini.com>
2018-09-10 15:08:55 +0530

    test locally

--- src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
+++ src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SmartbucketApplicationTests {
 
+	// new Test changes --2	
+	//Git changes locally
 	@Test
 	public void contextLoads() {
 	}
