a41eb622bf25c4e38b919cb6faf8c84efdcd5b67
Umesh Bharti <umesh.bharti@capgemini.com>
2018-09-12 16:36:28 +0530

    10 changes locally

--- src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
+++ src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmartbucketApplicationTests {
-	//99 locally 
+	//10 locally 
 	@Test
 	public void contextLoads() {
 	}
