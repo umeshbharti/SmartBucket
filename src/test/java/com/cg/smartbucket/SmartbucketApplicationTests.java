02492a653b673bcebae766f3eac9046bcb902289
Umesh Bharti <umesh.bharti@capgemini.com>
2018-09-12 16:43:25 +0530

    Revert "local changes 11"
    This reverts commit cbff1cadac53f573d4eae7fba9692bb863502d49.

--- src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
+++ src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmartbucketApplicationTests {
-	//11 locally 
+	//10 locally 
 	@Test
 	public void contextLoads() {
 	}
