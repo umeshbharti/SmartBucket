64aaf9728f4446c446c012db2b9c0524205bfa7b
umeshbharti <40784402+umeshbharti@users.noreply.github.com>
2018-09-17 14:32:33 +0530

    Update SmartbucketApplicationTests.java

--- src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
+++ src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmartbucketApplicationTests {
-// Gloabl changes
+// Gloabl changes 123
 /*	@Test
 	public void contextLoads() {
 	}*/
