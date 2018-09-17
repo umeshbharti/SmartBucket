144f2ea1e6a05d354d32f249868f5c5e39373af2
umeshbharti <40784402+umeshbharti@users.noreply.github.com>
2018-09-17 12:06:50 +0530

    changes 33 globally

--- src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
+++ src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SmartbucketApplicationTests {
 
 	//22 locally 
-	//22 Globally 
+	//33 Globally 
 /*	@Test
 	public void contextLoads() {
 	}*/
