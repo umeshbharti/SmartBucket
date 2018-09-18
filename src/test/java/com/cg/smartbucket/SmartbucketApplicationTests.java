43adf759635cbae34d5747502aa159b2a9e20617
umeshbharti <40784402+umeshbharti@users.noreply.github.com>
2018-09-18 14:55:53 +0530

    Gloabl changes 2

--- src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
+++ src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmartbucketApplicationTests {
-// Gloabl changes 1
+// Gloabl changes 2
 /*	@Test
 	public void contextLoads() {
 	}*/
