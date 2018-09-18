665a05a4caa84770a4bf21b49444f06cbd4122a9
umeshbharti <40784402+umeshbharti@users.noreply.github.com>
2018-09-18 10:14:56 +0530

    Gloabl changes 123456

--- src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
+++ src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmartbucketApplicationTests {
-// Gloabl changes 12345
+// Gloabl changes 123456
 /*	@Test
 	public void contextLoads() {
 	}*/
