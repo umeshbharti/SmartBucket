6f7a5dbaae19cd6080a143a5612371b898f5453b
Umesh Bharti <umesh.bharti@capgemini.com>
2018-09-18 14:47:18 +0530

    Umesh local changes 1

--- src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
+++ src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmartbucketApplicationTests {
-// Gloabl changes 123456789
+// Gloabl changes 1
 /*	@Test
 	public void contextLoads() {
 	}*/
