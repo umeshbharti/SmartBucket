9d8013c1aff8d4e8fda13d00d53c9817cf9835e1
umeshbharti <40784402+umeshbharti@users.noreply.github.com>
2018-09-24 14:03:15 +0530

    Test112345

--- src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
+++ src/test/java/com/cg/smartbucket/SmartbucketApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmartbucketApplicationTests {
-	//Test12345
+	//Test112345
 	@Test
 	public void contextLoads() {
 	}
